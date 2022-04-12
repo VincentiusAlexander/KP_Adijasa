cef65b69ef79b451eee62e2843c02caf4f3cd63f
Vincentius Alexander <vincentknights7@gmail.com>
2022-04-13 01:05:42 +0700

    fix error (done)

---
+++ Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Transaksi/PembayaranSewa/PembayaranSewa_Edit.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplikasi_Penitipan_Abu.Transaksi.PembayaranSewa
+{
+    /// <summary>
+    /// Interaction logic for PembayaranSewa_Edit.xaml
+    /// </summary>
+    public partial class PembayaranSewa_Edit : Window
+    {
+        public PembayaranSewa_Edit()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Transaksi/PenitipanAbu/PenitipanAbu_Form.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplikasi_Penitipan_Abu.Transaksi.PenitipanAbu
+{
+    /// <summary>
+    /// Interaction logic for PenitipanAbu_Form.xaml
+    /// </summary>
+    public partial class PenitipanAbu_Form : Window
+    {
+        public PenitipanAbu_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
