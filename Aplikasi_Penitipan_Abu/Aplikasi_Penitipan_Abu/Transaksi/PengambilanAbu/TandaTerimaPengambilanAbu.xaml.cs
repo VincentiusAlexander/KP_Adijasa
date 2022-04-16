e84716fb983263b01185521e8b04250de5894a31
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2022-04-16 10:35:13 +0700

    Tanda Terima Selesai (TAMPILAN)

---
+++ Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Transaksi/PengambilanAbu/TandaTerimaPengambilanAbu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplikasi_Penitipan_Abu.Transaksi.PengambilanAbu
+{
+    /// <summary>
+    /// Interaction logic for TandaTerimaPengambilanAbu.xaml
+    /// </summary>
+    public partial class TandaTerimaPengambilanAbu : Window
+    {
+        public TandaTerimaPengambilanAbu()
+        {
+            InitializeComponent();
+        }
+    }
+}
