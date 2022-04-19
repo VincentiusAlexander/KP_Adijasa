15f255973ec5de479b1e16da54f9bf020675045e
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2022-04-19 17:29:29 +0700

    Login System

--- Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Login.xaml.cs
+++ Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Login.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
+using MySql.Data.MySqlClient;
 
 namespace Aplikasi_Penitipan_Abu
 {
@@ -19,9 +21,53 @@ namespace Aplikasi_Penitipan_Abu
     /// </summary>
     public partial class Window1 : Window
     {
+        MySqlConnection conn;
         public Window1()
         {
             InitializeComponent();
+            conn = new MySqlConnection("datasource=localhost; port=3306; username=root; password=;database=penitipan_abu_adijasa");
+        }
+
+        public void login()
+        {
+            string password = getMD5Hash(txt_password.Password);
+            MySqlCommand cmd = new MySqlCommand("select * from users where username = ?username and password = ?password", conn);
+            cmd.Parameters.AddWithValue("?username", txt_username.Text);
+            cmd.Parameters.AddWithValue("?password", password);
+            conn.Close();
+            conn.Open();
+            Boolean isUser = false;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read()) if (reader.GetValue(2).ToString().Equals(password)) isUser = true;
+            conn.Close();
+            if (isUser)
+            {
+                MainWindow overview = new MainWindow();
+                overview.Show();
+                this.Close();
+                return;
+            }
+            System.Windows.Forms.MessageBox.Show("Gagal Login, periksa kembali username dan password", "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+        }
+
+        public string getMD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            login();
         }
     }
 }
