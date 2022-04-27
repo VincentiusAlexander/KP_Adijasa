77218b7cea2916a99205f208b64c10472c0dca5c
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2022-04-27 20:29:59 +0700

    sql e lupa :0

--- Aplikasi_Penitipan_Abu/penitipan_abu_adijasa.sql
+++ Aplikasi_Penitipan_Abu/penitipan_abu_adijasa.sql
@@ -31,9 +31,13 @@ CREATE TABLE `data_abu` (
   `tanggal_kremasi` datetime NOT NULL,
   `keterangan` longtext DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `data_abu` */
+
+insert  into `data_abu`(`id`,`nama_abu`,`nama_alternatif_abu`,`alamat_abu`,`jenis_kelamin`,`tanggal_lahir`,`tanggal_wafat`,`tanggal_kremasi`,`keterangan`) values 
+(1,'Hansen','Hansen','asd','Laki-laki','2000-03-08 00:00:00','2022-04-25 00:00:00','2022-04-25 00:00:00','ads'),
+(2,'asd','asd','asd','Laki-laki','2022-04-25 00:00:00','2022-04-25 00:00:00','2022-04-25 00:00:00','asd');
 
 /*Table structure for table `kategori` */
 
@@ -45,13 +49,13 @@ CREATE TABLE `kategori` (
   `harga` int(255) NOT NULL,
   `status` int(1) DEFAULT 0 COMMENT '0 == Not Deleted, 1 == Deleted',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `kategori` */
 
 insert  into `kategori`(`id`,`nama`,`harga`,`status`) values 
-(5,'Besar',10000,0),
-(6,'kecil',5000,0);
+(1,'Besar',15000,0),
+(2,'Kecil',12000,0);
 
 /*Table structure for table `kotak` */
 
@@ -62,10 +66,49 @@ CREATE TABLE `kotak` (
   `kategori_id` int(255) NOT NULL,
   `no_kotak` varchar(100) NOT NULL,
   `status` int(1) DEFAULT 0 COMMENT '0 == Not Deleted, 1 == Deleted',
+  `terpakai` int(1) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `kotak` */
+
+insert  into `kotak`(`id`,`kategori_id`,`no_kotak`,`status`,`terpakai`) values 
+(1,1,'A1',0,0),
+(2,1,'A2',0,0),
+(3,1,'A3',0,0),
+(4,1,'A4',0,0),
+(5,1,'A5',0,0),
+(6,1,'A6',0,0),
+(7,2,'B1',0,0),
+(8,1,'B2',0,0),
+(9,1,'B3',0,0),
+(10,1,'B4',0,0),
+(11,1,'B5',0,0),
+(12,1,'B6',0,0);
+
+/*Table structure for table `pembayaran_sewa` */
+
+DROP TABLE IF EXISTS `pembayaran_sewa`;
+
+CREATE TABLE `pembayaran_sewa` (
+  `id` int(11) DEFAULT NULL,
+  `id_penitipan` int(11) DEFAULT NULL,
+  `id_kotak` int(11) DEFAULT NULL,
+  `harga_kotak` int(11) DEFAULT NULL,
+  `harga_total_sewa` int(11) DEFAULT NULL,
+  `tanggal_awal` datetime DEFAULT NULL,
+  `tanggal_akhir` datetime DEFAULT NULL,
+  `status` int(1) DEFAULT 0,
+  KEY `id_penitipan` (`id_penitipan`),
+  KEY `id_kotak` (`id_kotak`),
+  CONSTRAINT `pembayaran_sewa_ibfk_1` FOREIGN KEY (`id_penitipan`) REFERENCES `penitipan` (`id`),
+  CONSTRAINT `pembayaran_sewa_ibfk_2` FOREIGN KEY (`id_kotak`) REFERENCES `kotak` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `pembayaran_sewa` */
+
+insert  into `pembayaran_sewa`(`id`,`id_penitipan`,`id_kotak`,`harga_kotak`,`harga_total_sewa`,`tanggal_awal`,`tanggal_akhir`,`status`) values 
+(0,1,8,15000,75000,'2022-04-26 00:00:00','2022-08-27 00:00:00',0);
 
 /*Table structure for table `penanggung_jawab` */
 
@@ -78,9 +121,14 @@ CREATE TABLE `penanggung_jawab` (
   `nomor_telp` varchar(55) NOT NULL,
   `relasi` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `penanggung_jawab` */
+
+insert  into `penanggung_jawab`(`id`,`nama`,`alamat`,`nomor_telp`,`relasi`) values 
+(1,'Mulya','ddd','2we13','Cek'),
+(2,'Chandra','ddas','2we13','Cek'),
+(3,'asd','asd','asd','asd');
 
 /*Table structure for table `penitipan` */
 
@@ -88,17 +136,21 @@ DROP TABLE IF EXISTS `penitipan`;
 
 CREATE TABLE `penitipan` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `tanggal_registrasi` datetime NOT NULL,
-  `tanggal_titip` datetime DEFAULT NULL,
-  `tanggal_ambil` datetime DEFAULT NULL,
+  `tanggal_registrasi` date NOT NULL,
+  `tanggal_titip` date DEFAULT NULL,
+  `tanggal_ambil` date DEFAULT NULL,
   `kotak_id` int(255) NOT NULL,
   `data_abu_id` int(11) NOT NULL,
   `penanggung_jawab_satu_id` int(11) NOT NULL,
   `penanggung_jawab_dua_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `penitipan` */
+
+insert  into `penitipan`(`id`,`tanggal_registrasi`,`tanggal_titip`,`tanggal_ambil`,`kotak_id`,`data_abu_id`,`penanggung_jawab_satu_id`,`penanggung_jawab_dua_id`) values 
+(1,'2022-04-25','2022-04-26','2027-04-26',8,1,1,2),
+(2,'2022-04-25','2022-04-25','2022-04-25',3,2,3,-1);
 
 /*Table structure for table `users` */
 
@@ -110,9 +162,13 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `role` int(1) NOT NULL COMMENT '1 = admin, 0 = staff',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`role`) values 
+(1,'test','098f6bcd4621d373cade4e832627b4f6',0),
+(2,'admin','21232f297a57a5a743894a0e4a801fc3',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
