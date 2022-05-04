3dd9d64df85905cb48f03b3d9a37fc7d9ae6de29
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2022-05-04 23:04:09 +0700

    Pembayaran Jaminan Edit

--- Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Transaksi/PembayaranJaminan/PembayaranJaminan_Edit.xaml.cs
+++ Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Transaksi/PembayaranJaminan/PembayaranJaminan_Edit.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,8 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Aplikasi_Penitipan_Abu.Transaksi.PembayaranJaminan
 {
@@ -20,9 +22,106 @@ namespace Aplikasi_Penitipan_Abu.Transaksi.PembayaranJaminan
     /// </summary>
     public partial class PembayaranJaminan_Edit : Page
     {
+        MySqlConnection conn;
+        int selectedId = -1;
         public PembayaranJaminan_Edit()
         {
             InitializeComponent();
+            populateComboBox();
+            conn = new MySqlConnection("datasource=localhost; port=3306; username=root; password=;database=penitipan_abu_adijasa");
+        }
+        private void populateComboBox()
+        {
+            cb_status.Items.Insert(0, "Belum Terbayar");
+            cb_status.Items.Insert(1, "Sudah Terbayar");
+            cb_status.Items.Insert(2, "Dikembalikan Pada Pemilik");
+            cb_status.SelectedIndex = 0;
+        }
+        private void btn_cari_jaminan_Click(object sender, RoutedEventArgs e)
+        {
+            PencarianPembayaranJaminan pencarian = new PencarianPembayaranJaminan();
+            pencarian.ShowDialog();
+            this.selectedId = pencarian.selectedId;
+            if (selectedId == -1)
+            {
+                System.Windows.Forms.MessageBox.Show("Pencarian Gagal, ulang kembali pencarian", "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+                return;
+            }
+            MySqlCommand cmd = new MySqlCommand("select * from jaminan where id = ?id", conn);
+            cmd.Parameters.AddWithValue("?id", selectedId);
+            conn.Close();
+            conn.Open();
+            int id_penitipan = -1;
+            int total_jaminan = -1;
+            int status = -1;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                id_penitipan = reader.GetInt32(1);
+                total_jaminan = reader.GetInt32(2);
+                if (reader.GetInt32(4) == 1)
+                {
+                    status = 2;
+                }else if (reader.GetInt32(3) == 0)
+                {
+                    status = 0;
+                }else if (reader.GetInt32(3) == 1)
+                {
+                    status = 1;
+                }
+            }
+            reader.Close();
+            uang_jaminan_txt.Text = "Rp." + total_jaminan.ToString();
+            no_kwitansi_jaminan_txt.Text = selectedId.ToString();
+            no_registrasi_txt.Text = id_penitipan.ToString();
+            cb_status.SelectedIndex = status;
+
+            cmd.Parameters.Clear();
+            cmd.CommandText = "select pj.nama from penitipan p left join penanggung_jawab pj on p.penanggung_jawab_satu_id = pj.id where p.id = ?id";
+            cmd.Parameters.AddWithValue("?id", id_penitipan);
+            reader = cmd.ExecuteReader();
+            string nama = "";
+            while (reader.Read())
+            {
+                nama = reader.GetString(0);
+            }
+            nama_penanggung_jawab_txt.Text = nama;
+            conn.Close();
+        }
+
+        private void btn_edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (check_box_centang_bila_data_benar.IsChecked == false)
+            {
+                System.Windows.Forms.MessageBox.Show("Centang Dahulu checkbox yang tersedia", "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+                return;
+            }
+            MySqlCommand cmd = new MySqlCommand("update jaminan set status = ?status, dikembalikan = ?dikembalikan where id = ?id", conn);
+            int status = -1;
+            int dikembalikan = -1;
+            if (cb_status.SelectedIndex == 0)
+            {
+                status = 0;
+                dikembalikan = 0;
+            }
+            else if (cb_status.SelectedIndex == 1)
+            {
+                status = 1;
+                dikembalikan = 0;
+            }
+            else if (cb_status.SelectedIndex == 2)
+            {
+                status = 1;
+                dikembalikan = 1;
+            }
+            cmd.Parameters.AddWithValue("?status", status);
+            cmd.Parameters.AddWithValue("?dikembalikan", dikembalikan);
+            cmd.Parameters.AddWithValue("?id", selectedId);
+            conn.Close();
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            System.Windows.Forms.MessageBox.Show("Berhasil Edit", "Success", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
         }
     }
 }
--- Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Transaksi/PencarianPembayaranJaminan.xaml.cs
+++ Aplikasi_Penitipan_Abu/Aplikasi_Penitipan_Abu/Transaksi/PencarianPembayaranJaminan.xaml.cs
@@ -36,7 +36,7 @@ namespace Aplikasi_Penitipan_Abu.Transaksi
         public void loadDataGrid(string filter = "")
         {
             data = new DataTable();
-            MySqlCommand cmd = new MySqlCommand("SELECT j.id AS 'ID', da.nama_abu AS 'Nama Abu', da.nama_alternatif_abu AS 'Nama Alternatif Abu', j.total_jaminan AS 'Total Jaminan', ps.tanggal_awal AS 'Tanggal Awal Penyimpanan', ps.tanggal_akhir AS 'Tanggal Akhir Penyimpanan', case when j.status = 0 then 'Belum Terbayar' when j.status = 1 then 'Sudah Terbayar' end as 'Status' FROM jaminan j LEFT JOIN penitipan p ON p.id = j.id_penitipan LEFT JOIN pembayaran_sewa ps ON p.id = ps.id_penitipan LEFT JOIN data_abu da ON da.id = p.data_abu_id where j.id like ?a or da.nama_abu like ?a or da.nama_alternatif_abu like ?a or j.total_jaminan like ?a or ps.tanggal_awal like ?a or ps.tanggal_akhir like ?a", conn);
+            MySqlCommand cmd = new MySqlCommand("SELECT j.id AS 'ID', da.nama_abu AS 'Nama Abu', da.nama_alternatif_abu AS 'Nama Alternatif Abu', j.total_jaminan AS 'Total Jaminan', ps.tanggal_awal AS 'Tanggal Awal Penyimpanan', ps.tanggal_akhir AS 'Tanggal Akhir Penyimpanan', case when j.dikembalikan = 1 then 'Dikembalikan pada Pemilik' when j.status = 0 then 'Belum Terbayar' when j.status = 1 then 'Sudah Terbayar' end as 'Status' FROM jaminan j LEFT JOIN penitipan p ON p.id = j.id_penitipan LEFT JOIN pembayaran_sewa ps ON p.id = ps.id_penitipan LEFT JOIN data_abu da ON da.id = p.data_abu_id where j.id like ?a or da.nama_abu like ?a or da.nama_alternatif_abu like ?a or j.total_jaminan like ?a or ps.tanggal_awal like ?a or ps.tanggal_akhir like ?a", conn);
             conn.Open();
             cmd.Parameters.AddWithValue("?a", "%" + filter + "%");
             conn.Close();
